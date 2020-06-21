eb82124caabdf220a9d81b6d22cada1851a3ed4b
Rachel Fritz <rachelfritz0@gmail.com>
2020-06-21 17:57:33 -0500

    new

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Array of special characters to be included
-var SpecialCharacters = [
-  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var SpecialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Array of numeric characters to be included
 var NumericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
